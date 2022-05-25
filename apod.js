d4e8e0777f305465e3d62fad007190fd99dc0dda
RESN036 <lhaisferreirada@gmail.com>
2022-05-25 18:13:43 -0300

    salvado a chave da api

---
+++ apod.js
@@ -0,0 +1,10 @@
+function nasa(){
+    let conteudo = $(`#data').val()
+    console.log(conteudo)
+    $.get({
+        type:`GET`,
+        url:https://api.nasa.gov/planetary/apod?api_key=L28hzMhIQpHep1FqXXJ1YeU0RF7ZM0JCalooy9Rk
+
+
+    })
+}
