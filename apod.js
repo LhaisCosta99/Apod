524a57690720f93f76fa4de9e1174a645a5d555f
RESN036 <lhaisferreirada@gmail.com>
2022-05-26 12:25:31 -0300

    começando o js

--- apod.js
+++ apod.js
@@ -1,10 +1,26 @@
-function nasa(){
-    let conteudo = $(`#data').val()
-    console.log(conteudo)
-    $.get({
-        type:`GET`,
-        url:https://api.nasa.gov/planetary/apod?api_key=L28hzMhIQpHep1FqXXJ1YeU0RF7ZM0JCalooy9Rk
+let pedido = (dateUsuario)=> {
+    $.ajax({
+        url: `https://api.nasa.gov/planetary/apod?api_key=L28hzMhIQpHep1FqXXJ1YeU0RF7ZM0JCalooy9Rk&date=${dateUsuario}`,
+        type: "GET",
+        success(resposta){
+            let imagem = resposta.url
+            let tit = resposta.title
+            let exp = resposta.explanation
 
 
-    })
+            displayN.append(`
+                <h2>${tit}</h2>
+                <img src = "${imagem}"
+                <p>${exp}</p>
+                
+            
+            `)
+
+
+            let divDis = $("#displayNasa");
+            divDis.css("display", "block");
+            
+    
+        }
+    });
 }
